69fadfe26c0032ec5e6afc5eb26b59a015898155
NaniChkhenkeli <130504824+NaniChkhenkeli@users.noreply.github.com>
2024-02-16 19:16:27 +0400

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,27 @@
+import datetime
+import pandas
+import random
+import smtplib
+
+MY_EMAIL = "chkhnnn@gmail.com"
+MY_PASSWORD = "abc213"
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"Letter_emplates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"], msg=f"Subject: happy birthday!\n\n{contents}")
+

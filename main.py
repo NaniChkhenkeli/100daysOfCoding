10e25bdfc705f8e218c7726ec6c6e62ba6ab3929
NaniChkhenkeli <130504824+NaniChkhenkeli@users.noreply.github.com>
2024-02-24 19:24:00 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import random
-import turtle as t
-
-tim = t.Turtle()
-
-colours = ["CornflowerBlue", "IndianRed", "DeepSkyBlue", "wheat", "SeaGreen"]
-def draw_shape(num_sides):
-    angle = 120 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+import requests
 
 
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
-
-
+requests.post()
